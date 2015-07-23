9029805528318f3f268054c18bab137b55ff736b
jobsyu <1175585627@qq.com>
2015-07-23 14:09:23 +0800

    Initial Commit

---
+++ UICollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollection
+//
+//  Created by jobs on 15/7/23.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollection
+//
+//  Created by jobs on 15/7/23.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
